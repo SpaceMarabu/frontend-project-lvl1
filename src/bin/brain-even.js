95a5371434c607df4ecf05a449f793688508573b
Daniil Klimov <qiberos@gmail.com>
2019-12-01 13:36:17 +0300

    stage 5

--- src/bin/brain-even.js
+++ src/bin/brain-even.js
@@ -1,13 +1,14 @@
 #!/usr/bin/env node
-import { isEven, random, game } from '../even';
+import game from '../even';
 import readlineSync from '../index';
 
 console.log('Welcome to the Brain Games!');
 console.log('Answer "yes" if the number is even, otherwise answer "no".');
+console.log();
 console.log('Welcome to the Brain Games!');
-export const userName = readlineSync.question('May I have your name? ');
+const userName = readlineSync.question('May I have your name? ');
 console.log(`Hi ${userName}!`);
 
 game();
 
-
+export { userName };
--- src/even.js
+++ src/even.js
@@ -1,24 +1,24 @@
 import readlineSync from './index';
 import { userName } from './bin/brain-even';
 
-const isEven = (num) => (num % 2 === 0) ? 'yes' : 'no';
+const isEven = (num) => ((num % 2 === 0) ? 'yes' : 'no');
 
 const random = () => Math.floor((Math.random() * 10) + (Math.random() * 10) + 1);
 
 const game = () => {
-for (let count = 0; count < 3; count += 1) {
-let question = random();
-let rightAnswer = isEven(question);
-console.log(`Question: ${question}`);
-let answer = readlineSync.question('Your answer: ');
-if (rightAnswer === answer) {
-console.log('Correct!');
-} else {
-console.log(`Let's try again, ${userName}!`);
-return count;
-};
-};
-console.log(`Congratulations, ${userName}!`);
+  for (let count = 0; count < 3; count += 1) {
+    const question = random();
+    const rightAnswer = isEven(question);
+    console.log(`Question: ${question}`);
+    const answer = readlineSync.question('Your answer: ');
+    if (rightAnswer === answer) {
+      console.log('Correct!');
+    } else {
+      console.log(`Let's try again, ${userName}!`);
+      return;
+    }
+  }
+  console.log(`Congratulations, ${userName}!`);
 };
 
-export { isEven, random, game };
+export default game;
